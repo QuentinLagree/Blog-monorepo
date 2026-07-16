9e5c9605304ca63a80f8ce046d0589d4ee64c3f8
QuentinLagree <lagreequentindev21@gmail.com>
2026-07-16 22:03:29 +0200

    Modification des switch boutons pour avoir une valeur par défault
    Feature : Optimisation + Ajout des préférences utilisateurs

--- web/src/app/shared/ui/form/switch-button/switch-button.ts
+++ web/src/app/shared/ui/form/switch-button/switch-button.ts
@@ -5,37 +5,54 @@ import {
   input,
   InputSignal,
   signal,
-  WritableSignal
+  WritableSignal,
 } from '@angular/core';
+import {
+  FormControl,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
 import { CheckboxConfig } from './models/checkbox-config';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-switch-button',
+  standalone: true,
   templateUrl: './switch-button.html',
   styleUrls: ['./switch-button.scss'],
-  imports: [ReactiveFormsModule]
+  imports: [
+    ReactiveFormsModule,
+  ],
 })
 export class SwitchButtonComponent {
-  @Input() control!: FormControl;
-  configuration: InputSignal<Partial<CheckboxConfig>> = input.required<Partial<CheckboxConfig>>();
-  localOverrides: WritableSignal<Partial<CheckboxConfig>> = signal({});
-  isChecked: WritableSignal<boolean> = signal<boolean>(false);
-
-  defaultConfiguration: CheckboxConfig = {
-    label: '',
+  @Input({
     required: true,
-    type: 'checkbox'
-  };
+  })
+  control: FormControl<boolean> = new FormControl(true, {
+      nonNullable: true,
+    });
+;
 
-  readonly mergeConfigs = computed<CheckboxConfig>(() => ({
-    ...this.defaultConfiguration,
-    ...(this.configuration() ?? {}),
-    ...(this.localOverrides() ?? {})
-  }));
-  toggleChecked() {
-    this.isChecked.set(!this.isChecked());
-    this.control.setValue(this.isChecked());
-    console.log(this.control);
-  }
+  readonly configuration:
+    InputSignal<Partial<CheckboxConfig>> =
+      input.required<
+        Partial<CheckboxConfig>
+      >();
+
+  readonly localOverrides:
+    WritableSignal<Partial<CheckboxConfig>> =
+      signal({});
+
+  readonly defaultConfiguration:
+    CheckboxConfig = {
+      label: '',
+      required: true,
+      type: 'checkbox',
+    };
+
+  readonly mergeConfigs =
+    computed<CheckboxConfig>(() => ({
+      ...this.defaultConfiguration,
+      ...(this.configuration() ?? {}),
+      ...(this.localOverrides() ?? {}),
+    }));
 }
