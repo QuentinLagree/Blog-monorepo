a54052d70873c63bb2dcf5a94e167f50500acacf
QuentinLagree <lagreequentindev21@gmail.com>
2026-08-04 02:59:05 +0200

    feat(post-card): add updated_at meta data

--- web/src/app/shared/ui/card/post-card/post-card.ts
+++ web/src/app/shared/ui/card/post-card/post-card.ts
@@ -1,6 +1,6 @@
 import { DatePipe } from "@angular/common";
 import { HttpContext } from "@angular/common/http";
-import { Component, effect, inject, input, InputSignal, output, signal, WritableSignal } from "@angular/core";
+import { Component, effect, inject, input, InputSignal, output, Signal, signal, WritableSignal } from "@angular/core";
 import { Router, RouterLink } from "@angular/router";
 import { SessionService } from "src/app/shared/services/session.service";
 import { User, UserService } from "src/app/shared/services/user.service";
@@ -15,7 +15,6 @@ import { PostService } from "src/app/features/posts/data-access/post.service";
 import { DangerButtonComponent } from "../../form/buttons/button-danger/button-danger";
 import { ContextMenuTriggerDirective } from "../../context-menu/context-menu.directive";
 import { AdminActionsMenu } from "../../context-menu/config/context-menu-options";
-import { ToastService } from "src/app/shared/helpers/toasts/toaster.service";
 import { ConfirmModalComponent } from "src/app/shared/helpers/modal/confirm-modal/confirm-modal";
 
 const SILENT_CONTEXT = new HttpContext().set(
@@ -24,57 +23,65 @@ const SILENT_CONTEXT = new HttpContext().set(
 );
 
 @Component({
-    selector: 'app-post-card',
-    templateUrl: 'post-card.html',
-    imports: [DatePipe, BaseButtonComponent, EditButtonComponent, RouterLink, DangerButtonComponent,
+  selector: 'app-post-card',
+  templateUrl: 'post-card.html',
+  imports: [DatePipe, BaseButtonComponent, EditButtonComponent, RouterLink, DangerButtonComponent,
     ContextMenuTriggerDirective, ConfirmModalComponent]
 })
 export class PostCard {
 
-    private _user: UserService = inject(UserService);
-    private _router: Router = inject(Router)
-    private _session: SessionService = inject(SessionService)
-    
-    post: InputSignal<Post> = input.required<Post>()
-    author: WritableSignal<User | undefined> = signal(undefined);
-    isDraft: InputSignal<boolean> = input(false)
-    likesCount = signal(0);
-    afterAction = output()
+  private _user: UserService = inject(UserService);
+  private _router: Router = inject(Router)
+  private _session: SessionService = inject(SessionService)
 
-    detailPath: string = "";
+  post: InputSignal<Post> = input.required<Post>()
+  author: WritableSignal<User | undefined> = signal(undefined);
+  isDraft: InputSignal<boolean> = input(false)
+  likesCount = signal(0);
+  afterAction = output()
 
-    isDeleted = signal(false)
+  detailPath: string = "";
+  isUpdated: WritableSignal<boolean> = signal(false);
 
-    authorLoading = false;
+  isDeleted = signal(false)
 
-    options = AdminActionsMenu
+  authorLoading = false;
 
-    sessionId = this._session.getUserIdSync();
-    sessionRole = this._session.getUserRoleSync();
+  options = AdminActionsMenu
 
-    isAdminAndNoAuthor: boolean = this.sessionId != this.author()?.id && this.sessionRole == 'admin'
-    
-   constructor (
+  sessionId = this._session.getUserIdSync();
+  sessionRole = this._session.getUserRoleSync();
+
+  isAdminAndNoAuthor: boolean = this.sessionId != this.author()?.id && this.sessionRole == 'admin'
+
+  constructor(
     private readonly _postService: PostService,
-   ) {
+  ) {
     effect(() => {
-        const currentPost = this.post()
-        const authorId = currentPost.authorId
-        this.detailPath = this.getDetailPath()
+      const currentPost = this.post()
+      const authorId = currentPost.authorId
+      this.detailPath = this.getDetailPath()
 
-        if (!authorId) {
-            this.author.set(undefined);
-            return;
-        }
+      if (!authorId) {
+        this.author.set(undefined);
+        return;
+      }
 
-        void this.loadAuthor(authorId)
+      void this.loadAuthor(authorId)
 
-        void this.loadLikeStatus(this.post()?.id ?? 0)
-        
+      void this.loadLikeStatus(this.post()?.id ?? 0)
+
+      const published_at: number = new Date(this.post().published_at).getTime();
+
+      if (new Date(this.post().updated_at ?? 0).getDay()) {
+        const updated_at = new Date(this.post().updated_at ?? 0).getTime() ?? 0;
+        this.isUpdated.set(updated_at >= published_at)
+      }
+
     })
-    }
+  }
 
-     private async loadAuthor(authorId: number): Promise<void> {
+  private async loadAuthor(authorId: number): Promise<void> {
     try {
       const context = new HttpContext()
         .set(SUCCESS_MESSAGE, false);
@@ -94,7 +101,7 @@ export class PostCard {
     }
   }
 
-  formatFromContextMenu = (_data: unknown, option: string) =>  {
+  formatFromContextMenu = (_data: unknown, option: string) => {
     switch (option) {
       case 'delete-post':
         console.log("OUVERTURE")
@@ -109,70 +116,70 @@ export class PostCard {
     // this.doAfterAction()
   }
 
-    getSlugifyPath (): string {
-       const slug = this.post().title
-    .normalize('NFC')
-    .toLocaleLowerCase('fr-FR')
-    .trim()
-    .replace(/['’`]+/gu, '-')
-    .replace(/[^\p{L}\p{N}]+/gu, '-')
-    .replace(/-+/g, '-')
-    .replace(/^-+|-+$/g, '');
+  getSlugifyPath(): string {
+    const slug = this.post().title
+      .normalize('NFC')
+      .toLocaleLowerCase('fr-FR')
+      .trim()
+      .replace(/['’`]+/gu, '-')
+      .replace(/[^\p{L}\p{N}]+/gu, '-')
+      .replace(/-+/g, '-')
+      .replace(/^-+|-+$/g, '');
 
-  return `${slug}-${this.post().id}`;
-    }
+    return `${slug}-${this.post().id}`;
+  }
 
-    getDetail () {
-        const detailPath = `/post/detail/${this.getSlugifyPath()}`
-        this._router.navigate([detailPath])
-        
-    }
+  getDetail() {
+    const detailPath = `/post/detail/${this.getSlugifyPath()}`
+    this._router.navigate([detailPath])
 
-    updatePost () {
-        const id = this.post().id;
-        const detailPath = `/post/edit/${id}`
-        this._router.navigate([detailPath])
-        
-    }
+  }
 
-    getDraftDetail () {
-        const detailPath = `/draft/detail/${this.getSlugifyPath()}`
-        this._router.navigate([detailPath])
-    }
+  updatePost() {
+    const id = this.post().id;
+    const detailPath = `/post/edit/${id}`
+    this._router.navigate([detailPath])
 
-    private getDetailPath () {
-        return `/post/detail/${this.getSlugifyPath()}`
-    }
+  }
 
-    private async loadLikeStatus(
-      postId: number,
-    ): Promise<void> {
-      try {
-        const response: Message<PostLikeStatus> =
-          await firstValueFrom(
-            this._postService.getStatusLike(postId, {
-              context: SILENT_CONTEXT,
-            }),
-          );
-    
-        if (!response.data) {
-          throw new Error(
-            "La réponse de l'API ne contient pas le statut du like.",
-          );
-        }
-    
-        this.likesCount.set(response.data.likesCount);
-      } catch (error) {
-        this.likesCount.set(0);
-    
-        console.error(
-          'Impossible de charger le statut du like.',
-          error,
+  getDraftDetail() {
+    const detailPath = `/draft/detail/${this.getSlugifyPath()}`
+    this._router.navigate([detailPath])
+  }
+
+  private getDetailPath() {
+    return `/post/detail/${this.getSlugifyPath()}`
+  }
+
+  private async loadLikeStatus(
+    postId: number,
+  ): Promise<void> {
+    try {
+      const response: Message<PostLikeStatus> =
+        await firstValueFrom(
+          this._postService.getStatusLike(postId, {
+            context: SILENT_CONTEXT,
+          }),
+        );
+
+      if (!response.data) {
+        throw new Error(
+          "La réponse de l'API ne contient pas le statut du like.",
         );
       }
-    }
 
-    publishModalOpen = false;
+      this.likesCount.set(response.data.likesCount);
+    } catch (error) {
+      this.likesCount.set(0);
+
+      console.error(
+        'Impossible de charger le statut du like.',
+        error,
+      );
+    }
+  }
+
+  publishModalOpen = false;
   publishModalLoading = false;
 
   openPostDeleteModal(): void {
@@ -217,7 +224,7 @@ export class PostCard {
     try {
 
       setTimeout(() => {
-      this.authorModalOpen = false;
+        this.authorModalOpen = false;
       }, 3000)
 
     } finally {
