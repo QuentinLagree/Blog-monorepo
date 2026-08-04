742cf85fd7b3cde21f4cb396096579c710461a01
QuentinLagree <lagreequentindev21@gmail.com>
2026-08-04 02:54:18 +0200

    refactor(header-composent): update of composant : add buttons and more flexible composant

--- web/src/app/shared/ui/content/header-content/header-content.ts
+++ web/src/app/shared/ui/content/header-content/header-content.ts
@@ -1,9 +1,13 @@
-import { Component, input, InputSignal, signal } from "@angular/core";
+import { Component, input, InputSignal, output, Output, OutputEmitterRef, signal } from "@angular/core";
+import { BaseButtonComponent, ButtonData } from "../../form/buttons/base-button";
+
 
 @Component({
   selector: 'app-header-content',
   standalone: true,
   template: `
+  @let actions = this.primaryAction && this.secondaryAction;
+  @let buttons = this.primaryButton() && this.secondaryButton();
   <header class="header-content">
     <div class="header-content__content">
       <p class="header-content__eyebrow">{{ this.eyebrow() }}</p>
@@ -14,15 +18,41 @@ import { Component, input, InputSignal, signal } from "@angular/core";
         {{ this.baseline() }}
       </p>
     </div>
-    <div class="header-content__actions">
-      <ng-content></ng-content>
+    @if (actions && buttons) {
+      <div class="header-content__actions">
+      <div class="btn-group btn-group--wrap">
+        <app-button
+          [icon]="this.primaryButton()!.icon ?? ''"
+          [label]="this.primaryButton()!.label"
+          (clicked)="doPrimaryAction()">
+        </app-button>
+
+        <app-button
+          type="secondary"
+          [icon]="this.secondaryButton()!.icon ?? ''"
+          [label]="this.secondaryButton()!.label"
+          (clicked)="doSecondaryAction()">
+        </app-button>
+      </div>
     </div>
+    }
   </header>
   `,
   styleUrls: ['./header-content.scss'],
+  imports: [BaseButtonComponent],
 })
 export class HeaderContentComponent {
   eyebrow: InputSignal<String> = input.required()
   title: InputSignal<String> = input.required()
   baseline: InputSignal<String> = input.required()
+
+  primaryButton: InputSignal<ButtonData | undefined> = input()
+  secondaryButton: InputSignal<ButtonData | undefined> = input()
+  
+  
+  primaryAction: OutputEmitterRef<void> = output<void>();
+  secondaryAction: OutputEmitterRef<void> = output<void>();
+
+  doPrimaryAction() { this.primaryAction.emit() }
+  doSecondaryAction() { this.secondaryAction.emit() }
 }
